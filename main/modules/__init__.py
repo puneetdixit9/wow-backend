32d55e1d80235d089981a3a77076fc8d71dd936d
Puneet Dixit <puneet.dixit@statusneo.com>
2023-08-20 16:44:46 +0530

    Desc: issue fixed

--- main/modules/__init__.py
+++ main/modules/__init__.py
@@ -1,11 +1,8 @@
 from flask_jwt_extended import JWTManager
 from flask_restx import Api
 
-from main.modules.jwt.controller import JWTController
-from main.modules.product_attribute_lableing.view import (
-    product_attribute_labelling_namespace,
-)
 from main.modules.auth.view import auth_namespace
+from main.modules.jwt.controller import JWTController
 from main.modules.wow.view import wow_namespace
 
 jwt = JWTManager()
@@ -13,7 +10,5 @@ api = Api()
 
 jwt.token_in_blocklist_loader(JWTController.token_revoked_check)
 
-
-api.add_namespace(product_attribute_labelling_namespace)
 api.add_namespace(auth_namespace)
 api.add_namespace(wow_namespace)
